e2c75c265efe246677b8476e186f425a9c3407ca
mytran123 <mytran091196@gmai.com>
2021-12-08 17:56:25 +0700

    update crud product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ProductRepository;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    protected $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function index()
+    {
+        $products = $this->productRepository->getAll();
+        return view("backend.product.list", compact("products"));
+    }
+
+    public function showFormCreate()
+    {
+        return view("backend.product.create");
+    }
+
+    public function create(Request $request)
+    {
+        $request->validate([
+            "name" => "required| max:20 | min:3",
+            "price" => "required"
+        ]);
+        $this->productRepository->create($request);
+        return redirect()->route('products.list');
+    }
+
+    public function showDetail($id)
+    {
+        $product = $this->productRepository->getById($id);
+        return view("backend.product.detail", compact("product"));
+    }
+
+    public function showFormEdit($id)
+    {
+        $product = $this->productRepository->getById($id);
+        return view("backend.product.edit",compact("product"));
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $this->productRepository->edit($request,$id);
+        return redirect()->route("products.list");
+    }
+
+    public function destroy($id)
+    {
+        $this->productRepository->delete($id);
+        return redirect()->route("products.list");
+    }
 }
