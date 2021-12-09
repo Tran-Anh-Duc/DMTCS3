a4a3a5dd0f58e9b08c34fac4278a930f3503296c
Nguyễn Mạnh Chí Thành <chithanhvtpt@gmail.com>
2021-12-09 09:57:45 +0700

    template complete

--- resources/views/backend/layout/navbar.blade.php
+++ resources/views/backend/layout/navbar.blade.php
@@ -14,15 +14,21 @@
         </div>
         <div class="avatar-dropdown" id="icon">
             <span>{{\Illuminate\Support\Facades\Auth::user()->name ?? ""}}</span>
-            <img {{\Illuminate\Support\Facades\Auth::user()->avatar ?? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqchnDJ6zreYOFN7Sag4iGQPEhjseY59SfQ-mf9bN2GpE8fcPXGGO3QC0YLVId0xw2zFU&usqp=CAU'}}">
+            <img
+                src="{{\Illuminate\Support\Facades\Auth::user()->avatar ?? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqchnDJ6zreYOFN7Sag4iGQPEhjseY59SfQ-mf9bN2GpE8fcPXGGO3QC0YLVId0xw2zFU&usqp=CAU'}}"
+                style="border-radius: 50%; width: 30px; height:30px">
         </div>
         <!-- Account dropdawn-->
         <ul class="mdl-menu mdl-list mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect mdl-shadow--2dp account-dropdown"
             for="icon">
             <li class="mdl-list__item mdl-list__item--two-line">
-                    <span class="mdl-list__item-primary-content">
-                        <span>Name  :  {{\Illuminate\Support\Facades\Auth::user()->name ?? ""}}</span>
-                        <span class="mdl-list__item-sub-title">{{\Illuminate\Support\Facades\Auth::user()->email ?? ""}}</span>
+                    <span class="mdl-list__item-primary-content row">
+                        <span><img
+                                src="{{\Illuminate\Support\Facades\Auth::user()->avatar ?? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqchnDJ6zreYOFN7Sag4iGQPEhjseY59SfQ-mf9bN2GpE8fcPXGGO3QC0YLVId0xw2zFU&usqp=CAU'}}"
+                                style="border-radius: 50%; width: 30px; height:30px"></span>
+                        <span>{{\Illuminate\Support\Facades\Auth::user()->name ?? ""}}</span>
+                        <span
+                            class="mdl-list__item-sub-title">{{\Illuminate\Support\Facades\Auth::user()->email ?? ""}}</span>
                     </span>
             </li>
             <li class="list__item--border-top"></li>
