7033d865db2b10b236459a33f64110fc5965db8a
Nguyễn Mạnh Chí Thành <chithanhvtpt@gmail.com>
2021-12-09 16:51:23 +0700

    8-4

--- resources/views/backend/product/order.blade.php
+++ resources/views/backend/product/order.blade.php
@@ -12,7 +12,7 @@
 <body>
 <div class="container">
     <div class="row">
-        <div class="col-lg-9">
+        <div class="col-lg-8">
             <table border="1px">
                 <thead>
                 <tr>
@@ -52,7 +52,7 @@
             </table>
         </div>
 {{--<collum 2 >--}}
-        <div class="col-lg-3">
+        <div class="col-lg-4">
             <table border="1px">
                 <thead>
                 <tr>
