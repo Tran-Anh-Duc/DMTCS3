d45f33635c3fc64f6ca66952e87438230afe2dce
Tran-Anh-Duc <trananhducty@gmail.com>
2021-12-10 10:23:05 +0700

    18

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -16,7 +16,7 @@ class StatusSeeder extends Seeder
     {
         DB::table('statuses')->insert([
             ["name" => "Active"],
-            ["name" => "Unactive"],
+            ["name" => "UnActive"],
         ]);
     }
 }
